c8f89a450b883ed93705d3547aac893ce36c34db
unknown <emilybtoliveira@gmail.com>
2019-06-14 16:16:47 -0300

    fifth commit

--- pagina_autenticada.php
+++ pagina_autenticada.php
@@ -3,7 +3,22 @@
   include('verificar_session.inc');
 ?>
 
-<h6>BEM-VINDO</h6>
-<form method="GET">
-  <input type="submit" name="logout" value="Sair">
-</form>
+<!DOCTYPE html>
+<html lang="pt-br">
+  <head>
+    <meta charset="utf-8">
+    <link href="style.css" rel="stylesheet">
+    <title>Index</title>
+  </head>
+  <body>
+    <div style="text-align: center;">
+      <p style="font-size: 25px;"><strong>BEM-VINDO</strong></p>
+      <form method="GET">
+        <input type="submit" name="logout" value="Sair" id="buttonSair">
+      </form>
+    </div>
+    <footer>
+      <t>Emily Brito de Oliveira - 914A</t>
+    </footer>
+  </body>
+</html>
